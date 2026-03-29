83235794efbd27589136f0d52d895f81821273e5
leeleeleeleejun <wnstjr6293@naver.com>
2026-03-30 01:14:02 +0900

    refactor: About 페이지 레이아웃 및 프로필 링크 제거

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -26,28 +26,12 @@ function Highlight({ children }: { children: string }) {
 export default function About() {
   return (
     <>
-      <div className="flex justify-around items-center">
-        <Link
-          href="https://www.notion.so/Front-end-fc7bab4a1bd14187a9693358b55bdfd1?pvs=4"
-          target="_blank"
-        >
-          <Image
-            src="/images/about/profile.jpg"
-            width={200}
-            height={200}
-            alt="profile"
-            priority={false}
-            placeholder={undefined}
-            className="rounded-full duration-300 hover:scale-110 max-md:w-[150px]"
-          />
-        </Link>
-        <div className="text-5xl text-center leading-relaxed max-md:text-4xl">
-          <div className="font-light">안녕하세요</div>
-          <strong>이준석</strong>
-          <div className="font-light">입니다</div>
-        </div>
+      <div className="flex gap-4 text-5xl text-center leading-relaxed max-md:text-4xl">
+        <div className="font-light">안녕하세요</div>
+        <strong>이준석</strong>
+        <div className="font-light">입니다</div>
       </div>
-      <div className="mt-[80px] mb-[30px] text-[17px] font-light tracking-tight leading-7">
+      <div className="mt-10 mb-[30px] text-[17px] font-light tracking-tight leading-7">
         <p>
           <Highlight>기본</Highlight>을 중요시하며
           <Highlight>효율적인 코드</Highlight>를 사랑합니다.
@@ -58,9 +42,6 @@ export default function About() {
           기본 개념과 동작 원리를 이해하면서 상황에 맞는 태그, 로직을 작성하기
           위해 노력합니다.
         </p>
-      </div>
-      <div className="font-light">
-        저에 대해 더 알고싶으시다면, 제 사진을 클릭해주세요!
       </div>
       <Skills />
     </>
